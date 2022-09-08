bcbe2400e3c80847a63393c6d10c5a01f3aa83e6
VITORIA CAROLINE NUNES FERREIRA <14156275680>
2022-09-08 19:35:24 -0300

    alteração exercicio 1

--- EstruturaRepetitiva/Exercicio1.java
+++ EstruturaRepetitiva/Exercicio1.java
@@ -6,21 +6,19 @@ public class Exercicio1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int X, Y;
-        System.out.println("Digite dois números:");
-        X = sc.nextInt();
-        Y = sc.nextInt();
+        int X = 0, Y = 0;
 
-        System.out.println("Digite outros dois números:");
-        X = sc.nextInt();
-        Y = sc.nextInt();
-
+       
         while (X != Y) {
             if (X > Y) {
+                System.out.println("Digite dois números:");
+                X = sc.nextInt();
+                Y = sc.nextInt();
                 System.out.print("DECRESCENTE!");
             } else {
                 System.out.print("CRESCENTE!");
             }
+
         }
 
         sc.close();
