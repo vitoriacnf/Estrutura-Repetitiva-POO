3b2de2f7f9659383a17b9b5804f6cfc786de43fb
Vitória Caroline <vitoriacnf5578@gmail.com>
2022-09-08 17:24:04 -0300

    exercicio 1

---
+++ EstruturaRepetitiva/Exercicio1.java
@@ -0,0 +1,28 @@
+package EstruturaRepetitiva;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int X, Y;
+        System.out.println("Digite dois números:");
+        X = sc.nextInt();
+        Y = sc.nextInt();
+
+        while (X != Y) {
+            if (X > Y) {
+                System.out.print("DECRESCENTE!");
+            } else {
+                System.out.print("CRESCENTE!");
+            }
+        }
+        System.out.println("Digite outros dois números:");
+        X = sc.nextInt();
+        Y = sc.nextInt();
+
+        sc.close();
+
+    }
+}
