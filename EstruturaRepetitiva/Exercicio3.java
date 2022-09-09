1fd79b92a261c22f298cdc05817d41f5c2aeaa9f
VITORIA CAROLINE NUNES FERREIRA <14156275680>
2022-09-08 22:00:39 -0300

    exercicio3

---
+++ EstruturaRepetitiva/Exercicio3.java
@@ -0,0 +1,24 @@
+package EstruturaRepetitiva;
+
+import java.util.Scanner;
+
+public class Exercicio3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int senha = 0;
+
+        System.out.print("Digite a senha: ");
+        senha = sc.nextInt();
+
+        while (senha != 0) {
+            System.out.println("Senha Inválida! Tente Novamente: ");
+            senha = sc.nextInt();
+        }
+        if (senha == 2002) {
+            System.out.println("Acesso permitido!");
+        }
+        
+        sc.close();
+    }
+}
