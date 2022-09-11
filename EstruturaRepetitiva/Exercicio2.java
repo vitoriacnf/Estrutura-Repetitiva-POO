5ee54d8f2e3c8e56245c218201f5de4638e5da05
Vitória Caroline <vitoriacnf5578@gmail.com>
2022-09-10 21:34:12 -0300

    correção

--- EstruturaRepetitiva/Exercicio2.java
+++ EstruturaRepetitiva/Exercicio2.java
@@ -14,22 +14,20 @@ public class Exercicio2 {
         System.out.println("Idades: ");
         idade = sc.nextInt();
 
-        while(idade > 0){
+        while (idade > 0) {
             somaIdades += idade;
             quantidadeIdades = quantidadeIdades + 1;
 
             idade = sc.nextInt();
         }
 
-        if(quantidadeIdades == 0){
+        if (quantidadeIdades == 0) {
             System.out.println("IMPOSSÍVEL CALCULAR");
-        }else{
-            Double media =  (somaIdades.doubleValue() / quantidadeIdades.doubleValue());
-            System.out.printf("soma idades: %d", somaIdades);
-            System.out.printf("quantidade idades: %d", quantidadeIdades);
-            System.out.printf("MEDIA = %.2f" , media);
+        } else {
+            Double media = (somaIdades.doubleValue() / quantidadeIdades.doubleValue());
+            System.out.printf("MEDIA = %.2f", media);
         }
-        
+
         sc.close();
     }
 }
